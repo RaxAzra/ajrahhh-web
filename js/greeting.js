fb735197e2bacca64590eb5d4fb1bbfff3401c27
Raxzra_ <97561664+RaxAzra@users.noreply.github.com>
2025-07-08 07:37:28 +0700

    Delete js directory

--- js/greeting.js
+++
@@ -1,7 +0,0 @@
-function getGreeting() {
-  const hour = new Date().getHours();
-  return hour >= 4 && hour < 10 ? "Good morning 👏🏻" :
-         hour >= 10 && hour < 15 ? "Good afternoon 👏🏻" :
-         hour >= 15 && hour < 18 ? "Good evening 👏🏻" :
-         "Good night 👏🏻";
-}
